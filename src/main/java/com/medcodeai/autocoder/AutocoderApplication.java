482a53a71d6b4152b6f81b62a261bc9d04157c20
Swapnil Railkar <swapnilrailkar01@gmail.com>
2025-05-29 16:36:28 +0530

    initial commit

---
+++ src/main/java/com/medcodeai/autocoder/AutocoderApplication.java
@@ -0,0 +1,13 @@
+package com.medcodeai.autocoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medcodeai/autocoder/AutocoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medcodeai.autocoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
